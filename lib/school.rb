10e367e85c0c5539329ea6c11fb7db9934d47c54
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-31 04:25:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student_name, grade)
+    @roster.each do |new_grade, new_name|
+      if new_grade == grade
+        @roster[grade] << student_name
+      else
+      @roster[grade] = []
+      @roster[grade] << student_name
+      end
+    end
+end
+end
