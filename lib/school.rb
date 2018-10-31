7613e7013eac848302ae01aee402496281dec873
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-31 04:34:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,12 @@
 # code here!
 
 class School
-
+  attr_accessor :name, :roster 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def roster
-    @roster
-  end
 
   def add_student(student_name, grade)
     @roster.each do |new_grade, new_name|
