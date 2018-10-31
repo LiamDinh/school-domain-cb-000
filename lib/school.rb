9f4a166410303984197837064540e6e153c0b2ea
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-31 04:35:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,13 +9,5 @@ class School
 
 
   def add_student(student_name, grade)
-    @roster.each do |new_grade, new_name|
-      if new_grade == grade
-        @roster[grade] << student_name
-      else
-      @roster[grade] = []
-      @roster[grade] << student_name
-      end
-    end
-end
+    
 end
